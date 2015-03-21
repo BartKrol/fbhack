8e0ab294ea413fbc3c74967c8655c5ab97631e99
Michael Lotkowski <michael@lotkowscy.co.uk>
2015-03-21 19:29:11 +0000

    chrome extension

--- extension/content.js
+++ extension/content.js
@@ -1,6 +1,20 @@
+(function () {
 
-$(document).ready(function () {
+    var script = document.createElement('script');
+    script.src = chrome.extension.getURL('/custom_js.js');
+    document.getElementsByTagName('head')[0].appendChild(script);
+    
+    setInterval(function () {
+        try {
 
-    $('#fbPhotoSnowliftViews').append('<span>I like trains!</span>');
+            if (document.getElementById('info-data') == null) {
+                var box = document.getElementById('fbPhotoSnowliftViews');
+                box.innerHTML += '<div id="info-data" style="background: rgb(228, 229,233); padding: 3px"><div><b>Info</b></div><div><button onclick="get_more_info()">Show Info</button></div></div>';
+            }
+        }
+        catch (e) {
+            //console.log(e);
 
-});
+        }
+    }, 1000);
+})();
