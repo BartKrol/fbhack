4f9becc8174791e80d4a4a7c85bb4a8f75f3d299
Michael Lotkowski <michael@lotkowscy.co.uk>
2015-03-22 03:21:09 +0000

    entity connection

--- server/app/main/views.py
+++ server/app/main/views.py
@@ -3,6 +3,8 @@ from . import main
 from app.camfind import *
 import json
 from app.freebase import Freebase
+from app.alchemy_api import AlchemyAPI
+from app.category_matcher import *
 
 @main.route("/")
 def hello():
@@ -27,3 +29,27 @@ def image():
         
         return json.dumps(response)
         #return 'HI'
+
+@main.route("entity", methods=['GET', 'POST'])
+def entity():
+    if request.method == 'GET':
+        
+        text = str(request.args.get('text'))
+        
+        alchemy = AlchemyAPI()
+
+        entities = alchemy.entities('text', text, {'sentiment': 0})
+        
+        #TODO: Think how to do this best
+        
+        for entity in entities['entities'][:1]:
+            freebase = Freebase(entity['text'])
+            html = freebase.get_html('london')
+
+            response = {'status': 'ok', 'html': html}
+            
+            
+
+        return json.dumps(response)
+        
+    
